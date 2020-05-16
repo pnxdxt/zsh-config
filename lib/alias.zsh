ae92abc9f8dfc7fd64789649425f06ed55a6ca45
Paul Nodet <paul.nodet@gmail.com>
2020-05-16 22:03:54 +0200

    Update

--- lib/alias.zsh
+++ lib/alias.zsh
@@ -47,6 +47,9 @@ alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resourc
 
 alias week='date +%V'          # Get week number
 alias cdate='date "+%d—%m—%Y"' # Get date
+alias timer='date1=`date +%s`; while true; do 
+   echo -ne "$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r";
+done'
 
 # Update installed Ruby gems, Homebrew, npm, and their installed packages
 alias brew_update="brew -v update; brew upgrade --force-bottle --cleanup; brew cleanup; brew cask cleanup; brew prune; brew doctor; npm-check -g -u"
