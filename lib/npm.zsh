b89e168f0635401b4b4f28355345263250d45ad2
Paul Nodet <5941125+pnxdxt@users.noreply.github.com>
2021-02-17 08:13:26 +0100

    Add : try fnm to replace? nvm

--- lib/npm.zsh
+++ lib/npm.zsh
@@ -6,6 +6,9 @@ export PATH="$PATH:$NPM_PACKAGES/bin"
 # Otherwise, fall back to `manpath` so we can inherit from `/etc/manpath`.
 export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
 
-export NVM_SYMLINK_CURRENT="true" # nvm use should make a symlink
-export NVM_DIR="$HOME/.nvm"
-export NVM_LAZY_LOAD=true
+# FNM
+export PATH="/var/folders/m2/r9_k9tb904x21r8zmdgs98br0000gn/T/fnm_multishell_38232_1613545934215/bin":$PATH
+export FNM_MULTISHELL_PATH="/var/folders/m2/r9_k9tb904x21r8zmdgs98br0000gn/T/fnm_multishell_38232_1613545934215"
+export FNM_DIR="/Users/pnodet/.fnm"
+export FNM_LOGLEVEL="info"
+export FNM_NODE_DIST_MIRROR="https://nodejs.org/dist"
