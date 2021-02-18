ed5f20d13d485b4a602e4ade217c058cedddfe89
Paul Nodet <5941125+pnxdxt@users.noreply.github.com>
2021-02-18 10:23:09 +0100

    Add : try fnm to replace? nvm

--- zinit.zsh
+++ zinit.zsh
@@ -9,19 +9,23 @@ zcommand() { zinit ice wait"0b" lucid as"command" "${@}"; }
 zload()    { zinit load                           "${@}"; }
 zsnippet() { zinit snippet                        "${@}"; }
 
-zinit ice wait"0a" atinit"ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay" atload"_zsh_highlight" lucid
-zinit load zsh-users/zsh-completions
-
 zinit ice as"program" make'!' atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' pick"direnv" src"zhook.zsh" lucid
 zinit light direnv/direnv
+
+# Prompt : https://github.com/starship/starship
+zinit ice from"gh-r" as"command" atload'eval "$(starship init zsh)"'
+zinit load starship/starship
+
+#zinit ice wait"0" lucid
+# must load it otherwise bindkeys won't work
+zinit light zsh-users/zsh-history-substring-search
 
 # A binary Zsh module which transparently and automatically compiles sourced scripts
 module_path+=( "${HOME}/.zinit/bin/zmodules/Src" )
 zmodload zdharma/zplugin &>/dev/null
 
-# Prompt : https://github.com/starship/starship
-zinit ice from"gh-r" as"command" atload'eval "$(starship init zsh)"'
-zinit load starship/starship
+zinit ice wait"0a" atinit"ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay" atload"_zsh_highlight" lucid
+zinit load zsh-users/zsh-completions
 
 zinit ice wait blockf lucid atpull'zinit creinstall -q .'
 zinit light zdharma/fast-syntax-highlighting
@@ -37,10 +41,3 @@ zinit light junegunn/fzf
 
 zinit ice wait"2" lucid
 zinit light djui/alias-tips
-
-zinit ice wait"2" lucid
-zinit light Aloxaf/fzf-tab
-
-#zinit ice wait"0" lucid
-# must load it otherwise bindkeys won't work
-zinit light zsh-users/zsh-history-substring-search
