dcb51438fc415423f947e32f471caf80bd675050
pnxdxt <paul.nodet@gmail.com>
2023-02-26 13:14:21 +0100

    update bootstrap to work on bash and remove zinit cloning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,20 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env sh
 
 ZSHCONFIG=${HOME}/.zsh-config
 
-function zsh_bootstrap() {
-    echo "Cloning zinit"
-    mkdir -p ~/.zinit
-    git clone https://github.com/zdharma-continuum/zinit.git ~/.zinit/bin
-
-    echo "Link resource files to ${HOME}"
-    ln -sf ${ZSHCONFIG}/zlogin ${HOME}/.zlogin
-    ln -sf ${ZSHCONFIG}/zprofile ${HOME}/.zprofile
-    ln -sf ${ZSHCONFIG}/zshenv ${HOME}/.zshenv
-    ln -sf ${ZSHCONFIG}/zshrc ${HOME}/.zshrc
-    echo "Done! Donwloading plugins, this may take a minute"
+zsh_bootstrap() {
+	echo "Link resource files to ${HOME}"
+	ln -sf ${ZSHCONFIG}/zlogin ${HOME}/.zlogin
+	ln -sf ${ZSHCONFIG}/zprofile ${HOME}/.zprofile
+	ln -sf ${ZSHCONFIG}/zshenv ${HOME}/.zshenv
+	ln -sf ${ZSHCONFIG}/zshrc ${HOME}/.zshrc
+	echo "Done! Donwloading plugins, this may take a minute"
 }
 
-function zsh_cleanup() {
-    echo "Clean up links to resource files at ${HOME}"
-    rm -f ${HOME}/.zlogin ${HOME}/.zprofile ${HOME}/.zshenv ${HOME}/.zshrc
-    echo "Done!"
+zsh_cleanup() {
+	echo "Clean up links to resource files at ${HOME}"
+	rm -f ${HOME}/.zlogin ${HOME}/.zprofile ${HOME}/.zshenv ${HOME}/.zshrc
+	echo "Done!"
 }
 
 zsh_cleanup && zsh_bootstrap
